5d7e3ba2fd5dc36e0634275d57f49998229a6bca
RV <sms.rajesh@gmail.com>
2022-09-30 00:30:00 +0530

    getting hyperbeam session

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,33 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import axios, { AxiosResponse } from 'axios';
+import { HBResponse } from './types/hb-response.type';
+
+const apiClient = axios.create({
+  baseURL: 'https://engine.hyperbeam.com',
+});
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly httpService: HttpService) {}
+
+  async computer(): Promise<HBResponse> {
+    try {
+      const resp = await apiClient.post<HBResponse>(
+        '/v0/vm',
+        {},
+        {
+          headers: {
+            Authorization: `Bearer sk_live_9chIoAfCklgfjS2-W5QktuKQr94X7JdZazQvYT-40o8`,
+          },
+        },
+      );
+      console.log(resp.data);
+      return resp.data;
+    } catch (error) {
+      console.log('Error: => ', error.response);
+      throw error;
+    }
   }
 }
---
+++ src/types/hb-response.type.ts
@@ -0,0 +1,5 @@
+export type HBResponse = {
+  session_id: string;
+  embed_url: string;
+  admin_token: string;
+};
